780ddcefd35412027e6ed275c7da8fa04016df20
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2023-02-17 15:16:23 +0100

    create calculator with addition method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcolatrice';
+  memo = '';
+  display = '';
+  risultato = 0;
+
+  addToMemo(event: any){
+    const labelDelBottone = event.target.value;
+    console.log(labelDelBottone);
+
+    this.memo += labelDelBottone;
+  }
+
+  clearAll(){
+    this.memo = "";
+    this.display = "";
+    this.risultato = 0;
+  }
+
+  addToDisplay(event: any){
+    this.display = this.memo;
+    this.memo = "";
+  }
+
+  calculateResult(event: any){
+
+    this.risultato += Number(this.display) + Number(this.memo);
+    this.memo = "";
+    this.display = String(this.risultato);
+  }
 }
